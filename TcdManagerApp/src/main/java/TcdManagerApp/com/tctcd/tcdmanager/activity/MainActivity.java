b9ff55f8414395a3aace6942fe9877160ce1521c
Li YANG <yang-li@tcl.com>
2018-05-22 16:22:35 +0800

    login and 注释

--- TcdManagerApp/src/main/java/TcdManagerApp/com/tctcd/tcdmanager/activity/MainActivity.java
+++ TcdManagerApp/src/main/java/TcdManagerApp/com/tctcd/tcdmanager/activity/MainActivity.java
@@ -12,20 +12,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import TcdManagerApp.com.tctcd.tcdmanager.R;
+import TcdManagerApp.com.tctcd.tcdmanager.tools.BmobTools;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private NavigationView navigationView;
+    private TextView username;
+    private TextView detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //设置actionbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //设置底部的那一个按钮以及绑定监听器
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,26 +42,36 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        //用drawerlayout 和 actionbardrawer 来实现侧滑效果
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //为侧滑出来的布局的item注册监听器
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //为侧滑出来的navigationview的上面的个人信息设置登陆用户text
+        View headerview = navigationView.inflateHeaderView(R.layout.nav_header_main);
+        username = headerview.findViewById(R.id.username);
+        detail = headerview.findViewById(R.id.details);
+        username.setText(BmobTools.userEntity.getUserName());
+        detail.setText(BmobTools.userEntity.getUserName());
     }
 
+    //点击返回按钮的事件
     @Override
     public void onBackPressed() {
+        //判断左边的是否拉出，如果是，则关闭，不是则不作处理
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
         }
     }
 
+    //设置右上角三点图标菜单
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -62,6 +79,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    //右上角菜单被选中的监听器
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -70,33 +88,37 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(this, "这是Settings", Toast.LENGTH_LONG).show();
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
+
+    //左边划开的菜单栏的item的监听器
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (id) {
+            case R.id.nav_camera:
+                break;
+            case R.id.nav_gallery:
+                break;
+            case R.id.nav_slideshow:
+                break;
+            case R.id.nav_manage:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_send:
+                break;
         }
-
+        //点击了某一个菜单之后关闭DrawerLayout
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
